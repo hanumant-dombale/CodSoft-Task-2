9817b7536dff849a9a036944aaa7133ef8302d0c
Hanumamt Dombale <hanumantdombale60@gmail.com>
2024-09-30 08:46:26 +0530

    update api

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "https://codsoft-task-2.onrender.com/api/user";
-export const JOB_API_END_POINT = "https://codsoft-task-2.onrender.com/api/job";
+export const USER_API_END_POINT = "http://localhost:8000/api/user";
+export const JOB_API_END_POINT = "http://localhost:8000/api/job";
 export const APPLICATION_API_END_POINT =
-    "https://codsoft-task-2.onrender.com/api/application";
-export const COMPANY_API_END_POINT = "https://codsoft-task-2.onrender.com/api/company";
+    "http://localhost:8000/api/application";
+export const COMPANY_API_END_POINT = "http://localhost:8000/api/company";
