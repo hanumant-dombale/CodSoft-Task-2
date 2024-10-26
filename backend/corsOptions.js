b8327fc9ab61f31cce571b8d9a73d318cb54c402
Hanumamt Dombale <hanumantdombale60@gmail.com>
2024-10-26 21:49:18 +0530

    fix error added origin

--- backend/corsOptions.js
+++ backend/corsOptions.js
@@ -1,7 +1,7 @@
 export const corsOptions = {
     origin: ["http://localhost:5173", 
         "https://jobbroad.netlify.app",
-        "https://test-v07q.onrender.com/"],
+        "https://test-v07q.onrender.com"],
 
     credentials: true,
 };
