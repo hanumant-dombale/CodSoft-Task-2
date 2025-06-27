a39baf485cb3cdceb10f17325202b32b7b65c587
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-06-27 22:50:41 +0530

    fix prodution issue

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-import { corsOptions } from "./corsOptions.js";
+// import cors from "cors";
+// import { corsOptions } from "./corsOptions.js";
 
 dotenv.config({});
 
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
