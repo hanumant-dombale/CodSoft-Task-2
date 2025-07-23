b4a13371433492bf64455081ed57ece3fdb0b874
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-07-23 22:26:32 +0530

    update lines

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,23 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import { corsOptions } from "./corsOptions.js";
+
+// imports all routes
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-import cors from "cors";
-import { corsOptions } from "./corsOptions.js";
-
 
 const app = express();
 
-app.use(express.json({limit: "20kb"}));
+app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
-	res.send("Job board backend run successfully");
+	res.send("😊😊 Job board backend run successfully 😊😊");
 });
 
 app.use((req, res, next) => {
@@ -24,9 +25,10 @@ app.use((req, res, next) => {
 	next();
 });
 
+// use all routes
 app.use("/api/user", userRoute);
 app.use("/api/application", applicationRoute);
 app.use("/api/job", jobRoute);
 app.use("/api/company", companyRoute);
 
-export default app ;
+export default app;
