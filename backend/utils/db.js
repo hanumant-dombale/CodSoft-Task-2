23eefa25c6cf392fcf2940ebb039aeaaa2a6a2fe
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-07-23 22:21:05 +0530

    Update log

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,13 @@
 import { connect } from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await connect(process.env.MONGO_URI);
-        console.log("mongodb connected successfully");
-    } catch (err) {
-        console.log(err);
-    }
+	try {
+		await connect(process.env.MONGO_URI);
+		console.log("🚀🚀 Mongodb Connected Successfully 🚀🚀");
+	} catch (error) {
+		console.log("❌❌ Database Connetion Error: ", error);
+		process.exit(1);
+	}
 };
+
 export default connectDB;
