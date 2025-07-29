453fc75e189f7e4279a5e73282450f2d9eff31ac
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-07-29 09:19:52 +0530

    update env variable name

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 const connectDB = async () => {
 	try {
-		await connect(process.env.MONGO_URI);
+		await connect(process.env.MONGODB_URI);
 		console.log("🚀🚀 Mongodb Connected Successfully 🚀🚀");
 	} catch (error) {
 		console.log("❌❌ Database Connetion Error: ", error);
