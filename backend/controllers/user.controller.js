03e69e734452507a7f62e34409e9b934eb490f6f
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-08-18 20:24:59 +0530

    update cookies issue

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -110,7 +110,8 @@ export const login = async (req, res) => {
 				maxAge: 1 * 24 * 60 * 60 * 1000,
 				httpOnly: true,
 				secure: process.env.NODE_ENV === "production",
-				sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+				sameSite: process.env.NODE_ENV === "production" ? "none" : "Strict",
+				path: "/",
 			})
 			.json({
 				message: `Welcome back ${user.fullname}. Your are login successfully.`,
@@ -134,7 +135,8 @@ export const logout = async (req, res) => {
 			.clearCookie("token", {
 				httpOnly: true,
 				secure: process.env.NODE_ENV === "production",
-				sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+				sameSite: process.env.NODE_ENV === "production" ? "none" : "Strict",
+				path: "/",
 			})
 			.json({
 				message: "User logged out successfully.",
