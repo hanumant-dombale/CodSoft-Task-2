34b1e16cbab805a0cc97c688d1da89aca98dbc57
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-08-19 08:00:55 +0530

    update logout function

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -131,7 +131,7 @@ export const logout = async (req, res) => {
 	try {
 		return res
 			.status(200)
-			.clearCookie("token", {
+			.cookie("token", token, {
 				httpOnly: true,
 				secure: process.env.NODE_ENV === "production",
 				sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
