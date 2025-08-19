af8acb090004572780f063e4c43b0738f92d782e
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-08-19 17:42:51 +0530

    update file code

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -129,11 +129,12 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
 	try {
-		res.clearCookie("token", {
+		res.cookie("token", "", {
+			maxAge: 1 * 24 * 60 * 60 * 1000,
 			httpOnly: true,
 			secure: process.env.NODE_ENV === "production",
 			sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-			path: "/", // must match login
+			path: "/",
 		});
 		return res.status(200).json({
 			message: "User logged out successfully.",
