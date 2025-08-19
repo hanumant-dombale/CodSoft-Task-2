8a6204b26045fd0d4b4bbe13486d724bab72fde5
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-08-19 18:50:29 +0530

    clear the local storage

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -8,7 +8,6 @@ import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { setUser } from "@/redux/authSlice";
 import { toast } from "sonner";
-import { persistor } from "./store";
 
 const Navbar = () => {
 	const { user } = useSelector((store) => store.auth);
@@ -24,7 +23,7 @@ const Navbar = () => {
 					withCredentials: true,
 				}
 			);
-			persistor.purge();
+			localStorage.removeItem("persist:root");
 			if (res.data.success) {
 				dispatch(setUser(null));
 				navigate("/");
