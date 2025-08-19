4a3ba01d0c594657237b0393b2c24d248161b89e
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-08-19 17:32:18 +0530

    fix issue

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -129,7 +129,12 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
 	try {
-		res.clearCookie("token");
+		res.clearCookie("token", {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+			path: "/", // must match login
+		});
 		return res.status(200).json({
 			message: "User logged out successfully.",
 			success: true,
